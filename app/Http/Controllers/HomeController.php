3437be4f916a0c3b0f4cb6bd8e734f6b14224ca8
Duque77 <jfduqueg1@eafit.edu.co>
2026-09-13 12:13:41 -0500

    Agrega busqueda de productos por nombre en la vista principal

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,19 +2,28 @@
 
 /**
  * Autor: Samuel Correa Velasquez (Desarrollador)
+ * Autor: Juan Fernando Duque (Desarrollador) - busqueda por nombre
  */
 
 namespace App\Http\Controllers;
 
 use App\Models\Producto;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    public function index(): View
+    public function index(Request $request): View
     {
+        $busqueda = trim((string) $request->query('buscar', ''));
+
+        $productos = Producto::with('categoria')
+            ->when($busqueda !== '', fn ($query) => $query->buscarPorNombre($busqueda))
+            ->get();
+
         return view('cliente.home', [
-            'productos' => Producto::with('categoria')->get(),
+            'productos' => $productos,
+            'busqueda' => $busqueda,
         ]);
     }
 }
